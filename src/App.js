5ff2dc28fdf98c3b5daa73e854d501762172a99c
Manjoot <manjoot@hotmail.com>
2020-04-14 15:35:47 +0100

    Adding dummy sentences to each page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,34 @@ const HomePage = () => {
   return (
     <div>
       <NavMenu />
+      <p>HOME: This is the home page.</p>
+    </div>
+  )
+}
+
+const AboutPage = () => {
+  return (
+    <div>
+      <NavMenu />
+      <p>ABOUT: This is the about page.</p>
+    </div>
+  )
+}
+
+const WorkoutsPage = () => {
+  return (
+    <div>
+      <NavMenu />
+      <p>WORKOUTS: This is the workouts page.</p>
+    </div>
+  )
+}
+
+const RecipesPage = () => {
+  return (
+    <div>
+      <NavMenu />
+      <p>RECIPES: This is the recipes page.</p>
     </div>
   )
 }
@@ -19,6 +47,9 @@ function App() {
   return (
     <Router>
       <HomePage path="/" />
+      <AboutPage path="/about" />
+      <WorkoutsPage path="/workouts" />
+      <RecipesPage path="/recipes" />
     </Router>
   );
 }
