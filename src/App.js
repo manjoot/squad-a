268c6431157207726ba5d272ac532a62ccfe0618
Manjoot <manjoot@hotmail.com>
2020-04-15 12:18:32 +0100

    Adding placeholder flexbox for two images (Still needs to be worked on)

--- src/App.js
+++ src/App.js
@@ -5,31 +5,46 @@ import './App.css';
 //Reach Router
 import { Router } from '@reach/router'
 
+//Material UI Grid for Two Images
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
 //Pages
 
 const HomePage = () => {
+  const classes = useStyles(); //Material UI grid style declaration
   return (
     <div className="content">
       <NavMenu />
-
       <div className="content-section welcome">
         <div className="header">
           <img src="https://files.slack.com/files-pri/TCX5TJ8DD-F0124RKC5Q9/nourish___lift-5.png" alt="cool" />
         </div>
       </div>
 
-      <div className="content-section workout">
-        <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-          Workout!
-        </h1>
-        <p>Workout Seciton</p>
-      </div>
-
-      <div className="content-section recipe">
-        <h1 style={{textAlign:"center", marginBottom: '1em'}}>
-          Recipe
-        </h1>
-        <p> blablalblabdsldsjdkdggdjsgsdkjlgsljfsdjlkg</p>
+      <div className="content-section">
+        <div className={classes.root}>
+          <Grid container spacing={3}>
+            <Grid item xs={6}>
+            <img className="image img1" src="https://files.slack.com/files-pri/TCX5TJ8DD-F011LSZ57CN/pull_up_-_happily_grey.jpg" alt="cool" />
+            </Grid>
+            <Grid item xs={6}>
+            <img className="image img2" src="https://files.slack.com/files-pri/TCX5TJ8DD-F0124SN7VK3/sesame-noodles-ingredients.jpg" alt="cool" />
+            </Grid>
+          </Grid>
+        </div>
       </div>
 
     </div>
