75d53fb06fa6466aec8a42b80604fdda41c616cb
Nilogen <Nilogen@hotmail.co.uk>
2020-04-15 09:53:33 +0100

    added RecipeReviewCard code and the components needed for it

---
+++ src/components/Recipes/Recipes.js
@@ -0,0 +1,12 @@
+import React from "react"
+import RecipeReviewCard from "./RecipeReviewCard"
+
+function Recipes(){
+    return(
+        <div>
+            <RecipeReviewCard></RecipeReviewCard>
+        </div>
+    )
+}
+
+export default Recipes
--- src/components/Recipes/RecipesAPI.js
+++ src/components/Recipes/RecipesAPI.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 
-function Recipes(){
-
+function RecipesAPI(){
+    return(
+        <div></div>
+    )
 } 
-
-export default Recipes
+export default RecipesAPI
