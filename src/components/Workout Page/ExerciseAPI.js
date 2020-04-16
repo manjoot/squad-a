8ae036ee6968352b72435aec0c97919837ea756a
Nilogen <Nilogen@hotmail.co.uk>
2020-04-16 11:35:06 +0100

    wrote the basic code to pull exercise name and to test it

---
+++ src/components/Workout Page/ExerciseAPI.js
@@ -0,0 +1,11 @@
+import React from "react"
+import axios from "axios"
+
+function ExceriseAPI(props){
+    const [exceriseName, setExceriseName]
+    return(
+
+    )
+}
+
+export default ExceriseAPI
