a45cce1eebacd301490605f5169acf546688cee3
Imah Farah <imahfarahh@gmail.com>
2020-04-16 14:44:46 +0100

    master

--- src/components/Workout Page/cardsection.js
+++ src/components/Workout Page/cardsection.js
@@ -176,6 +176,85 @@ export default function CardGrid(props) {
 
             </WorkoutCard>
         </Grid>
+        <Grid item xs={12} sm={4}>
+            <WorkoutCard 
+            nameOfExercise="Bye Bye Back Fat"
+            dateOfPublishing="20.04.2020"
+            titleOfExercise="Back"
+            shortDescription="Tackle those stubborn areas and lose back fat fast."
+            longDescription=
+            {<ol>
+            <li>Deadlift (3 sets, 45 seconds)</li>
+            
+            <li>Swimmers (3 sets, 45 seconds)</li>
+            
+            <li>Bent Over Rows (3 sets, 60 seconds)</li>
+            
+            <li>Plank Glute Kickback (3 sets, 60 seconds)</li>
+            
+            <li>Oblique Raises (3 sets, 30 seconds per side)</li>
+            
+            <li>Flutter Kicks (3 sets, 60 seconds)</li>
+
+            <li>Bent Over One Arms (3 sets, 60 seconds)</li>
+
+            <li>Burpees (3 sets, 45 seconds)</li>
+
+            <li>Bent Over Rows (3 sets, 60 seconds)</li>
+
+            <li>Plank (3 sets, 30 seconds per side)</li>
+            </ol>}></WorkoutCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+            <WorkoutCard 
+            nameOfExercise="Eliminate Side Fat"
+            dateOfPublishing="27.03.2020"
+            titleOfExercise="Mid-section"
+            shortDescription="Build your obliques and abs, become blown away by the results."
+            longDescription=
+            {<ol>
+            <li>Ab Bikes (3 sets, 60 seconds)</li>
+            
+            <li>Dumbbell Oblique Crunches (3 sets, 60 seconds)</li>
+            
+            <li>Glute Bridge to Leg Raises (3 sets, 45 seconds)</li>
+            
+            <li>Side Plank Crunches (3 sets, 60 seconds)</li>
+            
+            <li>Hip Thruster (3 sets, 60 seconds per side)</li>
+            
+            <li>Plank with Elevated Feet (3 sets, 45 seconds)</li>
+
+            <li>Side Crunch Kicks (3 sets, 45 seconds)</li>
+
+            <li>Crunches (3 sets, 60 seconds)</li>
+
+            <li>Side Plank (3 sets, 60 seconds)</li>
+
+            <li>Bodyweight Squat (3 sets, 60 seconds per side)</li>
+            </ol>}></WorkoutCard>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+            <WorkoutCard 
+            nameOfExercise="Arm Zapper"
+            dateOfPublishing="04.05.2020"
+            titleOfExercise="Arms"
+            shortDescription="Have super toned arms with 6 simple exercises."
+            longDescription=
+            {<ol>
+            <li>Bicep Curls (3 sets, 10 reps)</li>
+            
+            <li>Lateral Raises (3 sets, 5 reps)</li>
+            
+            <li>Arm Hold (3 sets, 10 second hold)</li>
+            
+            <li>Shoulder Presses (3 sets, 5 reps)</li>
+            
+            <li>Tricep Extensions (3 sets, 10 reps)</li>
+            
+            <li>Upright Rows (3 set, 10 reps)</li>
+            </ol>}></WorkoutCard>
+        </Grid>
       </Grid>
     </div>
   );
